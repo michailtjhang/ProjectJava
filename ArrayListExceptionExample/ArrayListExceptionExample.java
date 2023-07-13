d1134bb2ad2399f5f3409d3c446cca478fc95140
MICHAIL <72181056+michailtjhang@users.noreply.github.com>
2023-07-13 08:44:49 +0700

    Add files via upload

---
+++ ArrayListExceptionExample/ArrayListExceptionExample.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ContohArrayListException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        try {
+            System.out.print("Masukkan 5 angka: ");
+            for (int i = 0; i < 5; i++) {
+                int number = scanner.nextInt();
+                numbers.add(number);
+            }
+
+            System.out.println("Angka-angka yang dimasukkan:");
+            for (int number : numbers) {
+                System.out.println(number);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Terjadi kesalahan saat memasukkan input. Pastikan Anda memasukkan angka.");
+        } finally {
+            scanner.close();
+        }
+    }
+    
+}
