e3cdf19986cf150fb820cab3f401cb9f2f57d773
MICHAIL <72181056+michailtjhang@users.noreply.github.com>
2023-07-13 09:00:12 +0700

    Add files via upload

---
+++ 02_Text/Text.java
@@ -0,0 +1,18 @@
+package pkg2_text;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Halo Apa Kabara ?");
+        System.out.println("Baik baik saja");
+        
+        System.out.print("Bagimana Kabar");
+        System.out.print("kamu sendiri ?");
+        
+        System.out.println(" ");
+        System.out.println("2 + 4");
+        System.out.println(2 + 4);
+        // ketik sout + teken tab
+    }
+    
+}
